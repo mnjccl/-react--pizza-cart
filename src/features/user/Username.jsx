2e5e658595c4b47bc5e62a5683a451909878dc4b
Minja Ćućula <cuculaminja@proton.me>
2024-08-21 18:33:25 +0200

    Initial commit

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from 'react-redux';
+
+function Username() {
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="text-semibold hidden text-sm md:block">{username}</div>
+  );
+}
+
+export default Username;
